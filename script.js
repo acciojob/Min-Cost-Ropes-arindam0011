6b952b03cc20b0e66c45ccaf86aa383332bb8d46
ARINDAM BHANDARI <136267807+arindam0011@users.noreply.github.com>
2024-05-21 02:24:01 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,7 +2,13 @@ function mincost(arr)
 { 
 //write your code here
 // return the min cost
-  
+	let sum=0;
+	let minc=0;
+	for(let i=1; i< arr.lenght-1; i++){
+		sum= sum+arr[i]+arr[i-1];
+		minc+=sum;
+	}
+  return minc;
 }
 
 module.exports=mincost;
